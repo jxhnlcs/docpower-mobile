eb08a50dd89650663f9d24dd196f78b91ce31526
jxhnlcs <joohnzay@gmail.com>
2023-06-08 21:15:28 -0300

    Update server.js

--- TecerApp/api/server.js
+++ TecerApp/api/server.js
@@ -29,25 +29,65 @@ app.use(express.json());
 
 // Rota de autenticação de login
 app.post('/login', (req, res) => {
-    const { cpf, senha } = req.body;
-  
-    // Verifique as credenciais do cliente no banco de dados
-    const query = 'SELECT * FROM cliente WHERE cpf = ? AND senha = ?';
-    connection.query(query, [cpf, senha], (error, results) => {
-      if (error) {
-        console.error('Erro ao verificar as credenciais:', error);
-        res.status(500).json({ message: 'Erro ao verificar as credenciais' });
+  const { cpf, senha } = req.body;
+
+  // Verifique as credenciais do cliente no banco de dados
+  const query = 'SELECT * FROM cliente WHERE cpf = ? AND senha = ?';
+  connection.query(query, [cpf, senha], (error, results) => {
+    if (error) {
+      console.error('Erro ao verificar as credenciais:', error);
+      res.status(500).json({ message: 'Erro ao verificar as credenciais' });
+    } else {
+      if (results.length > 0) {
+        // Login bem-sucedido
+        res.status(200).json({ message: 'Login bem-sucedido' });
       } else {
-        if (results.length > 0) {
-          // Login bem-sucedido
-          res.status(200).json({ message: 'Login bem-sucedido' });
-        } else {
-          // Login inválido
-          res.status(401).json({ message: 'CPF ou senha inválidos' });
-        }
+        // Login inválido
+        res.status(401).json({ message: 'CPF ou senha inválidos' });
       }
-    });
-  });  
+    }
+  });
+});  
+
+//   // Rota de autenticação de login
+// app.post('/login', (req, res) => {
+//   const { cpf, senha } = req.body;
+
+//   // Verifique as credenciais do cliente no banco de dados
+//   const query = 'SELECT * FROM cliente WHERE cpf = ? AND senha = ?';
+//   connection.query(query, [cpf, senha], (error, results) => {
+//     if (error) {
+//       console.error('Erro ao verificar as credenciais:', error);
+//       res.status(500).json({ message: 'Erro ao verificar as credenciais' });
+//     } else {
+//       if (results.length > 0) {
+//         // Login bem-sucedido
+//         const clienteId = results[0].id;
+
+//         // Consulta para obter os documentos associados ao cliente e suas categorias
+//         const documentosQuery = `
+//           SELECT d.id, c.nome AS nome_cliente, cat.nome AS nome_categoria, d.nome AS nome_documento
+//           FROM documentos d
+//           JOIN cliente c ON d.cliente_id = c.id
+//           JOIN categorias cat ON d.categoria_id = cat.id
+//           WHERE d.cliente_id = ?
+//         `;
+//         connection.query(documentosQuery, [clienteId], (error, documentoResults) => {
+//           if (error) {
+//             console.error('Erro ao consultar os documentos:', error);
+//             res.status(500).json({ message: 'Erro ao consultar os documentos' });
+//           } else {
+//             // Documentos encontrados
+//             res.status(200).json({ documentos: documentoResults });
+//           }
+//         });
+//       } else {
+//         // Login inválido
+//         res.status(401).json({ message: 'CPF ou senha inválidos' });
+//       }
+//     }
+//   });
+// }); 
 
 // Iniciando o servidor
 app.listen(port, () => {
